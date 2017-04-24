226b639999dad3c7e6449e6737ae3a93665aa2c8
jalmond <jalmond@cern.ch>
2017-04-24 20:20:00 +0900

    update listmaker

--- CatProducer/prod/makelist_807fromjson.py
+++ CatProducer/prod/makelist_807fromjson.py
@@ -1,14 +1,13 @@
 import os,sys
 
 datasetname = [ 
-    ["DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8" , ""]
+    ["TTToSemilepton_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8" , "170423_160142"]
     ]
 
 catversion="v8-0-7"
 readfromcatversion="v8-0-6"
 newfilename=""
 user=os.getenv("USER")
-user="jskim"
 
 os.system("bash /cvmfs/cms.cern.ch/crab3/crab.sh ")
 
@@ -24,27 +23,35 @@ for m in range(0, len(datasetname)):
     if os.path.exists("list.txt"):
         os.system("rm list.txt")
     
-    print "grep " + pdataset + " /cms/scratch/SNU/datasets_"+readfromcatversion+"/*.txt | grep catTuple_1.root > list.txt"
-    os.system("grep " + pdataset + " /cms/scratch/SNU/datasets_"+readfromcatversion+"/*.txt | grep catTuple_1.root > list.txt")
     
-    readlist= open("list.txt","r")
-    fornewfile=[]
-    for r in readlist:
-        r=r.replace(":", " ")
-        sr = r.split()
-        oldfile= sr[0]
-        print "reading from " + oldfile
+    filefound=False
+    nfile=0
+    while not filefound:
+        nfile=nfile+1
+        print "grep " + pdataset + " /cms/scratch/SNU/datasets_"+readfromcatversion+"/*.txt | grep catTuple_"+str(nfile)+".root > list.txt"
+        os.system("grep " + pdataset + " /cms/scratch/SNU/datasets_"+readfromcatversion+"/*.txt | grep catTuple_"+str(nfile)+".root > list.txt")
+            
+        readlist= open("list.txt","r")
+        fornewfile=[]
+        for r in readlist:
+            if ".root" in r:
+                filefound=True
+            r=r.replace(":", " ")
+            sr = r.split()
+            oldfile= sr[0]
+            print "reading from " + oldfile
+            
+            if not os.path.exists(oldfile):
+                sys.exit()
 
-        if not os.path.exists(oldfile):
-            sys.exit()
-
-        readoldfile = open(oldfile,"r")
-        newfilename = oldfile.replace(readfromcatversion,catversion)
-        for ro in readoldfile:
-            if not ".root" in ro:
-                fornewfile.append(ro)
-                print (ro.split())
-
+            readoldfile = open(oldfile,"r")
+            newfilename = oldfile.replace(readfromcatversion,catversion)
+            for ro in readoldfile:
+                if not ".root" in ro:
+                    fornewfile.append(ro)
+                    print (ro.split())
+        readlist.close()
+        
     os.system("ls -lth  /xrootd/store/user/"+user+"/"+pdataset+ "  > log1.txt" )
 
     readlog1 = open("log1.txt","r")
@@ -67,6 +74,10 @@ for m in range(0, len(datasetname)):
          readlog1b.close()
          os.system("rm log1b.txt")
          print "prodtag = " + prodtag
+    if not os.path.exists( "/xrootd/store/user/"+user+"/"+pdataset + "/"+MiniAODCampaign + "/"+prodtag):
+        print "prodtag = " + prodtag + " is wrong"
+        sys.exit()
+
     os.system("ls -lth  /xrootd/store/user/"+user+"/"+pdataset + "/"+MiniAODCampaign + "/" + prodtag + " > log1b.txt")
     
     readlog1b = open("log1b.txt","r")
@@ -79,7 +90,12 @@ for m in range(0, len(datasetname)):
     readlog1b.close()
     os.system("rm log1b.txt")
     
-               
+    print newfilename
+
+    for l in nlists:
+        print "Adding  /xrootd/store/user/"+user+"/"+pdataset + "/" +MiniAODCampaign   + "/" + prodtag + "/"+l
+
+    filelist=[]
     for l in nlists:
         if os.path.exists("log2.txt"):
             os.system("rm log2.txt")
@@ -87,23 +103,28 @@ for m in range(0, len(datasetname)):
         os.system("ls -lth  /xrootd/store/user/"+user+"/"+pdataset + "/" +MiniAODCampaign   + "/" + prodtag + "/"+l+"/ > log2.txt")
 
         readlog2 = open("log2.txt","r")
-        listfile = open(newfilename,"w")
-        for coline in fornewfile:
-            if "path " in coline:
-                listfile.write("# path =  /xrootd/store/user/"+user+"/"+pdataset + "/"+MiniAODCampaign + "/" + prodtag + "\n" )
-            else:
-                listfile.write(coline)
             
         for line in readlog2:
             if "bestman" in line:
                 sline =line.split()
                 if len(sline) == 9:
                     filename=sline[8]
-                    listfile.write("root://cms-xrdr.sdfarm.kr:1094///xrd/store/user/"+user+"//"+pdataset + "/"+ MiniAODCampaign + "/" + prodtag + "/"+ l + " /"+filename+"\n") 
+                    filelist.append("root://cms-xrdr.sdfarm.kr:1094///xrd/store/user/"+user+"//"+pdataset + "/"+ MiniAODCampaign + "/" + prodtag + "/"+ l + "/"+filename+"\n") 
+
         readlog2.close()
-        listfile.close()
+
         os.system("rm log2.txt")
         os.system("chmod 777 " + newfilename)
+
+    listfile = open(newfilename,"w")
+    for coline in fornewfile:
+        if "path " in coline:
+            listfile.write("# path =  /xrootd/store/user/"+user+"/"+pdataset + "/"+MiniAODCampaign + "/" + prodtag + "\n" )
+        else:
+            listfile.write(coline)
+    for fileline in filelist:
+        listfile.write(fileline)
+    listfile.close()
     os.system("rm log1.txt")
 
     print "--"*50
